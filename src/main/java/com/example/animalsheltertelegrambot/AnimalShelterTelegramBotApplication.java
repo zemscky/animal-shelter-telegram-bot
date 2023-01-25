91d5b8e2049f1b79ab2092448fbafed5ad7358d3
zemscky <aleksei.zemscky@yandex.ru>
2023-01-25 13:24:19 +0300

    Commit

---
+++ src/main/java/com/example/animalsheltertelegrambot/AnimalShelterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalsheltertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/animalsheltertelegrambot/AnimalShelterTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalsheltertelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
