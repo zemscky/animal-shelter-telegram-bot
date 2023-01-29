2332e0893b37fc18c4d2080890b186105942b076
zemscky <aleksei.zemscky@yandex.ru>
2023-01-29 18:14:26 +0300

    Заполнил поля у классов Animal и Client добавил геттеры, сеттеры, equals, hashCode и toString к данным классам.

--- src/main/java/com/example/animalsheltertelegrambot/models/Animal.java
+++ src/main/java/com/example/animalsheltertelegrambot/models/Animal.java
@@ -2,12 +2,82 @@ package com.example.animalsheltertelegrambot.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Animal {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String name;
+    private String color;
+    public String animalType;
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return id == animal.id && age == animal.age && Objects.equals(name, animal.name) && Objects.equals(color, animal.color) && Objects.equals(animalType, animal.animalType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color, animalType, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", animalType='" + animalType + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/com/example/animalsheltertelegrambot/models/Client.java
+++ src/main/java/com/example/animalsheltertelegrambot/models/Client.java
@@ -2,12 +2,82 @@ package com.example.animalsheltertelegrambot.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Client {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private String name;
+    private String address;
+    private int age;
+    private int telephoneNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id == client.id && age == client.age && telephoneNumber == client.telephoneNumber && Objects.equals(name, client.name) && Objects.equals(address, client.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, age, telephoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                ", telephoneNumber=" + telephoneNumber +
+                '}';
+    }
 }
