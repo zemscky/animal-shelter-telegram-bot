e221283d38fb46818714a78b13897c51c4f90a32
nikitamavrodiy <mna1998@mail.ru>
2023-01-29 18:28:20 +0300

    Добавил аннотации @Repository и создал репозиторий ShelterMessageRepository.java

--- src/main/java/com/example/animalsheltertelegrambot/repositories/ClientRepository.java
+++ src/main/java/com/example/animalsheltertelegrambot/repositories/ClientRepository.java
@@ -2,6 +2,8 @@ package com.example.animalsheltertelegrambot.repositories;
 
 import com.example.animalsheltertelegrambot.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
--- src/main/java/com/example/animalsheltertelegrambot/repositories/ShelterMessageRepository.java
+++ src/main/java/com/example/animalsheltertelegrambot/repositories/ShelterMessageRepository.java
@@ -1,9 +1,10 @@
 package com.example.animalsheltertelegrambot.repositories;
 
-import com.example.animalsheltertelegrambot.models.Animal;
+import com.example.animalsheltertelegrambot.models.ShelterMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
+public interface ShelterMessageRepository extends JpaRepository<ShelterMessage, Long> {
+
 }
