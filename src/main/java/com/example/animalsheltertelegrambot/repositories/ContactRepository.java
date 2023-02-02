03606b646c04fa89fe907b446264e2d384c72652
nikitamavrodiy <mna1998@mail.ru>
2023-02-02 15:09:18 +0300

    Создан объект и репозиторий Contact для callback запросов

--- src/main/java/com/example/animalsheltertelegrambot/repositories/ContactRepository.java
+++ src/main/java/com/example/animalsheltertelegrambot/repositories/ContactRepository.java
@@ -1,9 +1,9 @@
 package com.example.animalsheltertelegrambot.repositories;
 
-import com.example.animalsheltertelegrambot.models.InfoMessage;
+import com.example.animalsheltertelegrambot.models.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InfoMessageRepository extends JpaRepository<InfoMessage, String> {
+public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
